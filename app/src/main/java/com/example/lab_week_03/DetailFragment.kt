d22b43f0d8d52561405d6e4bdf619eeee9884d3f
oliviakarissakristanti <koliviakarissa@gmail.com>
2025-09-14 21:24:11 +0700

    Commit No. 3 – use dynamic fragment

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -26,6 +26,12 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
+
     fun setCoffeeData(id: Int) {
         when (id) {
             R.id.affogato -> {
@@ -41,5 +47,16 @@ class DetailFragment : Fragment() {
                 coffeeDesc?.text = getString(R.string.latte_desc)
             }
         }
+    }
+
+    companion object {
+        private const val COFFEE_ID = "COFFEE_ID"
+
+        fun newInstance(coffeeId: Int) =
+            DetailFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(COFFEE_ID, coffeeId)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -5,8 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
 
-// Interface supaya ListFragment bisa komunikasi ke MainActivity
 interface CoffeeListener {
     fun onSelected(id: Int)
 }
@@ -17,16 +17,31 @@ class MainActivity : AppCompatActivity(), CoffeeListener {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Tambahkan ListFragment saat pertama kali dibuka
+        if (savedInstanceState == null) {
+            findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction()
+                    .add(containerLayout.id, listFragment)
+                    .commit()
+            }
+        }
     }
 
     override fun onSelected(id: Int) {
-        val detailFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_detail) as DetailFragment
-        detailFragment.setCoffeeData(id)
+        // Ganti ListFragment dengan DetailFragment
+        findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
+            val detailFragment = DetailFragment.newInstance(id)
+            supportFragmentManager.beginTransaction()
+                .replace(containerLayout.id, detailFragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 }
